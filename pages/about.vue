04dde01b33ce7ef619cec33a3ee0cfa7fdbc60ae
coredelle-sv <coredelle.smith@softvision.com>
2024-02-20 15:55:47 -0500

    revert sample code and add tailwind again

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,9 @@
 <template>
   <div>This is my page.</div>
-  <div>
-    <button @click="enableCustomLayout">Update Layout</button>
-  </div>
 </template>
 
 <script setup>
-function enableCustomLayout() {
-  setPageLayout("another");
-}
-// definePageMeta({
-//   layout: "another",
-// });
+definePageMeta({
+  layout: "another",
+});
 </script>
