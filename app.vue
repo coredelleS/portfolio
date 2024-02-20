0ac584d6832717f087d3affc316c0488145a6ee5
coredelle-sv <coredelle.smith@softvision.com>
2024-02-20 15:38:47 -0500

    one way to add layout other than default

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <NuxtLayout>
+  <NuxtLayout :name="layout">
     <!-- nuxtPage is nuxt's way of showing current page which defaults to (index.vue) page in pages folder -->
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+const layout = 'another';
+</script>
---
+++ layouts/another.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        This is another layout
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
