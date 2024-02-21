826628b1bb00259682757474fea27e78fde0e4ab
coredelle-sv <coredelle.smith@softvision.com>
2024-02-21 13:12:38 -0500

    add projects link and fetch data, conditional rendering based on data fetching

---
+++ pages/projects.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2 class="text-2xl gont-semibold mb-10">Projects</h2>
+        <ProjectList />
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
