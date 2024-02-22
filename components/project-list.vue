f0944e19cccbd80afcb20fceca479dd82bd06416
coredelle-sv <coredelle.smith@softvision.com>
2024-02-22 10:38:39 -0500

    add stargazer count and description

--- components/project-list.vue
+++ components/project-list.vue
@@ -10,7 +10,13 @@
         class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
       >
         <a :href="repository.html_url" target="_blank">
-          <div class="font-semibold">{{ repository.name }}</div>
+          <div class="flex items-center justify-between text-sm">
+            <div class="font-semibold">{{ repository.name }}</div>
+            <div>{{ repository.stargazers_count }} ★</div>
+          </div>
+          <p class="text-sm">
+            {{ repository.description }}
+          </p>
         </a>
       </li>
     </ul>
