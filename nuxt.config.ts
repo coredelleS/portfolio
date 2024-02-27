dc08cb0381ccfd3adcfdc1225eb39ecb4bfe0506
coredelle-sv <coredelle.smith@softvision.com>
2024-02-27 11:49:24 -0500

    add md with code snippet and photo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/content"],
+  content: {
+    highlight: {
+      theme: {
+        default: 'min-light',
+        dark: 'min-dark'
+      }
+    }
+  },
   colorMode: {
     classSuffix: "",
   },
